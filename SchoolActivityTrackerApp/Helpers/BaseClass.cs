0b627ced8bebcadfbbd434bb4f5c912eeca4d4ba
Mukizar5 <mukizar5@gmail.com>
2024-04-17 00:14:37 +0300

    method creation for creating tasks, displaying complete  and incomplete tasks

---
+++ SchoolActivityTrackerApp/Helpers/BaseClass.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SchoolActivityTrackerApp.Helpers
+{
+    public class BaseClass
+    {
+        public class BaseClass
+        {
+            public Guid Id { get; set; } = Guid.NewGuid();
+            public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+            public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
+            public bool IsDeleted { get; set; }
+        }
+    }
+}
